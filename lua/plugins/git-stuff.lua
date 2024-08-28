2ec523f3b866a477dd7d4e517f0d1dfdcf7c12d6
lmckenzie99 <157838863+lmckenzie99@users.noreply.github.com>
2024-08-28 16:43:34 -0400

    added keybinds for vim fugitive for pull merge and add

--- lua/plugins/git-stuff.lua
+++ lua/plugins/git-stuff.lua
@@ -1,17 +1,20 @@
 return {
 	{
 		"tpope/vim-fugitive",
-        config = function ()
-           vim.keymap.set("n", "<Leader>gp", ":Git push<CR>", {})
-            vim.keymap.set("n", "Leader>gc", "Git commit<CR>",{})
-        end
+		config = function()
+			vim.keymap.set("n", "<Leader>gp", ":Git push<CR>", {})
+			vim.keymap.set("n", "<Leader>gc", ":Git commit<CR>", {})
+			vim.keymap.set("n", "<Leader>gl", ":Git pull<CR>", {})
+			vim.keymap.set("n", "<Leader>gm", ":Git merge<CR>", {})
+            vim.keymap.set("n", "<Leader>ga", ":Git add .<CR>", {})
+		end,
 	},
 
 	{
 		"lewis6991/gitsigns.nvim",
-		 config = function()
+		config = function()
 			require("gitsigns").setup()
-			vim.keymap.set("n", "<Leader>gp", ":Gitsigns preview_hunk<CR>", {})
-		end
-	}
+			vim.keymap.set("n", "<Leader>gh", ":Gitsigns preview_hunk<CR>", {})
+		end,
+	},
 }
