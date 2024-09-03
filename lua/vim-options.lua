72ad9d7007bfb13b0221d8989203bd8bd8dcc895
lmckenzie99 <157838863+lmckenzie99@users.noreply.github.com>
2024-09-02 23:06:04 -0400

    added bad keybinds, get over it. also added nvim-autopairs. config WIP.

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -8,6 +8,10 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.api.nvim_set_keymap("i", "<C-s>", "<ESC>:w<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<C-s>", "<ESC>:wa<CR>", {})
-vim.keymap.set({"n", "i"}, "<C-u>", "<ESC>:u<CR>", {})
+vim.keymap.set({ "n", "i" }, "<C-u>", "<ESC>:u<CR>", {})
 
-
+--yeah I remapped hjkl, get over it
+vim.keymap.set("n", "h", "<LEFT>", {})
+vim.keymap.set("n", "j", "<UP>", {})
+vim.keymap.set("n", "l", "<RIGHT>", {})
+vim.keymap.set("n", "k", "<DOWN>", {})
