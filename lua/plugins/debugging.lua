f1da828bcb6f94b868f593ae5f5ce898627a436a
lmckenzie99 <157838863+lmckenzie99@users.noreply.github.com>
2024-09-10 12:14:10 -0400

    formatting

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -3,9 +3,8 @@ return {
 	dependencies = {
 		"rcarriga/nvim-dap-ui",
 		"nvim-neotest/nvim-nio",
-	
-},
-  
+	},
+
 	config = function()
 		local dap = require("dap")
 		local dapui = require("dapui")
