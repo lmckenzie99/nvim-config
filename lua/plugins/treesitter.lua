9dacfca2aaa194c9692f954cd0b1c8d29438566c
lmckenzie99 <157838863+lmckenzie99@users.noreply.github.com>
2024-09-10 12:17:19 -0400

    added rust to require install treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
         local config = require("nvim-treesitter.configs")
         config.setup({
             auto_install = true,
-            ensure_installed = {"lua", "cpp", "python"},
+            ensure_installed = {"lua", "cpp", "python", "rust"},
             highlight = { enable = true },
             indent = { enable = true },
 
