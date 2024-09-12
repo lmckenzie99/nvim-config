9a6d10c674ee1a560549e1886280f520b116a635
lmckenzie99 <157838863+lmckenzie99@users.noreply.github.com>
2024-09-12 13:13:47 -0400

    setup comment toggling for highlighted area

--- lua/plugins/nvim-commenter.lua
+++ lua/plugins/nvim-commenter.lua
@@ -1,3 +1,9 @@
-return{
-  {'terrortylor/nvim-comment'},
+return {
+	{
+		"terrortylor/nvim-comment",
+		config = function()
+			require("nvim_comment").setup({ comment_empty = false, comment_empty_trim_whitespace = false })
+			vim.keymap.set("v", "<Leader>cc", ":CommentToggle<CR>", {})
+		end,
+	},
 }
