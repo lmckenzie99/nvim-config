95c781db1ca0ed5d785873f9cb42379a75863242
lmckenzie99 <157838863+lmckenzie99@users.noreply.github.com>
2024-09-19 21:16:21 -0400

    added indent line coloring support via catppuccin

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,105 +1,110 @@
 return {
-	{
-		"catppuccin/nvim",
-		lazy = false,
-		name = "catppuccin",
-		priority = 1000,
-		config = function()
-			require("catppuccin").setup({
-				latte = require("catppuccin.palettes").get_palette("latte"),
-				frappe = require("catppuccin.palettes").get_palette("frappe"),
-				macchiato = require("catppuccin.palettes").get_palette("macchiato"),
-				mocha = require("catppuccin.palettes").get_palette("mocha"),
-				flavour = "auto", -- latte, frappe, macchiato, mocha
-				background = { -- :h background
-					light = "latte",
-					dark = "mocha",
-				},
-				transparent_background = false, -- disables setting the background color.
-				show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-				term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
-				dim_inactive = {
-					enabled = false, -- dims the background color of inactive window
-					shade = "dark",
-					percentage = 0.15, -- percentage of the shade to apply to the inactive window
-				},
-				no_italic = false, -- Force no italic
-				no_bold = false, -- Force no bold
-				no_underline = false, -- Force no underline
-				styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-					comments = { "italic" }, -- Change the style of comments
-					conditionals = { "italic" },
-					loops = {},
-					functions = {},
-					keywords = {},
-					strings = {},
-					variables = {},
-					numbers = {},
-					booleans = {},
-					properties = {},
-					types = {},
-					operators = {},
-					-- miscs = {}, -- Uncomment to turn off hard-coded styles
-				},
-				color_overrides = {
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        latte = require("catppuccin.palettes").get_palette("latte"),
+        frappe = require("catppuccin.palettes").get_palette("frappe"),
+        macchiato = require("catppuccin.palettes").get_palette("macchiato"),
+        mocha = require("catppuccin.palettes").get_palette("mocha"),
+        flavour = "auto", -- latte, frappe, macchiato, mocha
+        background = { -- :h background
+          light = "latte",
+          dark = "mocha",
+        },
+        transparent_background = false, -- disables setting the background color.
+        show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+        term_colors = false,        -- sets terminal colors (e.g. `g:terminal_color_0`)
+        dim_inactive = {
+          enabled = false,          -- dims the background color of inactive window
+          shade = "dark",
+          percentage = 0.15,        -- percentage of the shade to apply to the inactive window
+        },
+        no_italic = false,          -- Force no italic
+        no_bold = false,            -- Force no bold
+        no_underline = false,       -- Force no underline
+        styles = {                  -- Handles the styles of general hi groups (see `:h highlight-args`):
+          comments = { "italic" },  -- Change the style of comments
+          conditionals = { "italic" },
+          loops = {},
+          functions = {},
+          keywords = {},
+          strings = {},
+          variables = {},
+          numbers = {},
+          booleans = {},
+          properties = {},
+          types = {},
+          operators = {},
+          -- miscs = {}, -- Uncomment to turn off hard-coded styles
+        },
+        color_overrides = {
 
-					mocha = {
-						rosewater = "#F5B8AB",
-						flamingo = "#F29D9D",
-						pink = "#AD6FF7",
-						mauve = "#FF8F40",
-						red = "#E66767",
-						maroon = "#EB788B",
-						peach = "#FAB770",
-						yellow = "#faca64",
-						green = "#70CF67",
-						teal = "#4CD4BD",
-						sky = "#61BDFF",
-						sapphire = "#4BA8FA",
-						blue = "#00BFFF",
-						lavender = "#00BBCC",
-						text = "#C1C9E6",
-						subtext1 = "#A3AAC2",
-						subtext0 = "#8E94AB",
-						overlay2 = "#7D8296",
-						overlay1 = "#676B80",
-						overlay0 = "#464957",
-						surface2 = "#3A3D4A",
-						surface1 = "#2F313D",
-						surface0 = "#1D1E29",
-						base = "#0b0b12",
-						mantle = "#11111a",
-						crust = "#191926",
-					},
-				},
-				custom_highlights = {},
-				default_integrations = true,
-				integrations = {
+          mocha = {
+            rosewater = "#F5B8AB",
+            flamingo = "#F29D9D",
+            pink = "#AD6FF7",
+            mauve = "#FF8F40",
+            red = "#E66767",
+            maroon = "#EB788B",
+            peach = "#FAB770",
+            yellow = "#faca64",
+            green = "#70CF67",
+            teal = "#4CD4BD",
+            sky = "#61BDFF",
+            sapphire = "#4BA8FA",
+            blue = "#00BFFF",
+            lavender = "#00BBCC",
+            text = "#C1C9E6",
+            subtext1 = "#A3AAC2",
+            subtext0 = "#8E94AB",
+            overlay2 = "#7D8296",
+            overlay1 = "#676B80",
+            overlay0 = "#464957",
+            surface2 = "#3A3D4A",
+            surface1 = "#2F313D",
+            surface0 = "#1D1E29",
+            base = "#0b0b12",
+            mantle = "#11111a",
+            crust = "#191926",
+          },
+        },
+        custom_highlights = {},
+        default_integrations = true,
+        integrations = {
           alpha = true,
           harpoon = true,
           rainbow_delimiters = true,
           nvimtree = true,
           semantic_tokens = false,
           ts_rainbow = true,
-					cmp = true,
-					gitsigns = true,
-					neotree = true,
-					treesitter = true,
-					notify = false,
-					mini = {
-						enabled = true,
-						indentscope_color = "",
-					},
-					telescope = {
-						enabled = true,
-						style = "mocha",
-					},
-					-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-				},
-			})
+          cmp = true,
+          gitsigns = true,
+          neotree = true,
+          treesitter = true,
+          notify = false,
+          mini = {
+            enabled = true,
+            indentscope_color = "",
+          },
+          telescope = {
+            enabled = true,
+            style = "mocha",
+          },
+          indent_blankline = {
+            enabled = true,
+            scope_color = "mocha", -- catppuccin color (eg. `lavender`) Default: text
+            colored_indent_levels = false,
+          },
+          -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+        },
+      })
 
-			-- setup must be called before loading
-			vim.cmd.colorscheme("catppuccin")
-		end,
-	},
+      -- setup must be called before loading
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
 }
