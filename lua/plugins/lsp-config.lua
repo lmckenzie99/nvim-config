dee267937c87563469adf19cb4259981553ff5a8
lmckenzie99 <157838863+lmckenzie99@users.noreply.github.com>
2024-11-18 13:28:22 -0500

    edits

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,53 +1,54 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        lazy = false,
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "jay-babu/mason-nvim-dap.nvim",
-        event = "VeryLazy",
-        dependencies = {
-        "williamboman/mason.nvim",
-            "mfussenegger/nvim-dap",
-        },
-        opts = {
-            handlers = {}
-        },
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"mfussenegger/nvim-dap",
+		},
+		opts = {
+			handlers = {},
+		},
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities =
+				require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+			-- Setup required for ufo
 
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        lazy = false,
-        opts = {
-            auto_install = true,
-        },
-    },
-    {
-        "neovim/nvim-lspconfig",
-        lazy = false,
-        config = function()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.ts_ls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.pylsp.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.clangd.setup({
-                capabilities = capabilities,
-            })
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-        end,
-    },
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.pylsp.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.clangd.setup({
+				capabilities = capabilities,
+			})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -10,9 +10,10 @@ vim.api.nvim_set_keymap("i", "<C-s>", "<ESC>:w<CR>", { noremap = true, silent = 
 vim.api.nvim_set_keymap("n", "<C-s>", "<ESC>:wa<CR>", {})
 vim.keymap.set({ "n", "i" }, "<C-u>", "<ESC>:u<CR>", {})
 
-
 vim.keymap.set("n", "<Leader>id", ":IBLDisableScope<CR>", {})
 vim.keymap.set("n", "<Leader>ie", ":IBLEnableScope<CR>", {})
-vim.api.nvim_set_keymap("n", "<Leader>cr", ":!g++ % -o temp<CR>", {noremap = true, silent = true})
-vim.cmd [[command! Qa :qa]]
-vim.cmd [[command! Q :q]]
+vim.api.nvim_set_keymap("n", "<Leader>co", ":!g++ % -o temp<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Leader>cr", ":split<CR> :terminal<CR> i ./temp<CR>", { noremap = true, silent = true })
+
+vim.cmd([[command! Qa :qa]])
+vim.cmd([[command! Q :q]])
